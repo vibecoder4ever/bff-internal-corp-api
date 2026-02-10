7649a33ffcc08791e494de07fde5c447b8207c0b
vibecoder4ever <trainingbreach@gmail.com>
2026-02-10 08:54:39 -0500

    Remove swagger doc

--- management-api.js
+++ management-api.js
@@ -1,27 +1,7 @@
 const router = express.Router();
 
-/**
- * @swagger
- * /api/v1/bff-internal-corp-api/update-data:
- *   post:
- *     summary: "Internal Use"
- *     description: "Internal data management endpoint."
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *     responses:
- *       200:
- *         description: "OK"
- *       500:
- *         description: "Error"
- */
 router.post('/update-data', (req, res) => {
-
     return res.status(403).send('Internal only');
-
 });
 
 module.exports = router;
