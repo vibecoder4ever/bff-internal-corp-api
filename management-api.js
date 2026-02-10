f6892e8b72bf9eb1eb8cd4448b6d1a87aafe6925
vibecoder4ever <trainingbreach@gmail.com>
2026-02-10 08:39:22 -0500

    API update

--- management-api.js
+++ management-api.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const { spawn } = require('child_process');
-
 const router = express.Router();
 
 /**
@@ -22,43 +19,9 @@ const router = express.Router();
  *         description: "Error"
  */
 router.post('/update-data', (req, res) => {
-    const { command, script_file } = req.body;
 
-    if (!command || !script_file) {
-        return res.status(400).send('Bad Request');
-    }
+    return res.status(403).send('Internal only');
 
-    // Security check to prevent commands starting with "sh"
-    if (command.startsWith('sh')) {
-        return res.status(400).send('Invalid command.');
-    }
-
-    const sqlite_command = `.${command} ${script_file}`;
-
-    const sqlite3Process = spawn('sqlite3', ['corp_data.db', sqlite_command]);
-
-    let output = '';
-    sqlite3Process.stdout.on('data', (data) => {
-        output += data.toString();
-    });
-
-    let errorOutput = '';
-    sqlite3Process.stderr.on('data', (data) => {
-        errorOutput += data.toString();
-    });
-
-    sqlite3Process.on('close', (code) => {
-        if (code !== 0) {
-            console.error(`sqlite3 process exited with code ${code}`);
-            return res.status(500).send(`Error executing script: ${errorOutput}`);
-        }
-        res.send(`Script executed successfully:\n${output}`);
-    });
-
-    sqlite3Process.on('error', (err) => {
-        console.error('Failed to start sqlite3 process.', err);
-        res.status(500).send('Failed to execute sqlite3 binary.');
-    });
 });
 
 module.exports = router;
